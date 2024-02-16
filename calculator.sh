ee2997702573d30e03bbaadcd5d462fb38cebbd8
Nishtha Paul <33231250+nishthapaul@users.noreply.github.com>
2024-02-16 15:52:59 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -12,13 +12,13 @@ echo "4. Multiplication:"
 #read ch
 
 case $ch in
-  1)res=`expr$a + $b`
+  1)res=`expr $a + $b`
   ;;
-  2)res=`expr$a - $b`
+  2)res=`expr $a - $b`
   ;;
-  3)res=`expr$a / $b`
+  3)res=`expr $a / $b`
   ;;
-  4)res=`expr$a \* $b`
+  4)res=`expr $a \* $b`
   ;;
 esac
 echo "Result: $res"
